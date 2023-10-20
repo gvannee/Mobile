09e4a306d8e6020d168f8b8361026bf829d141ac
gvannee <mph.gvan@gmail.com>
2023-10-20 22:13:52 +0700

    using ROOM

--- app/src/main/java/hanu/a2_2001040222/recyleview/MainActivity.kt
+++ app/src/main/java/hanu/a2_2001040222/recyleview/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import hanu.a2_2001040222.recyleview.adapter.FoodAdapter
 import hanu.a2_2001040222.recyleview.appDatabase.AppDatabase
+import hanu.a2_2001040222.recyleview.databinding.ActivityMainBinding
 import hanu.a2_2001040222.recyleview.model.Food
 import hanu.a2_2001040222.recyleview.viewModel.MainViewModel
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
         val db = Room.databaseBuilder(
@@ -26,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         val appDao = db.appDao()
         val viewModel = MainViewModel(appDao)
 
+
+
         val list = mutableListOf<Food>(
             Food(name = "bun", price = 10000),
             Food(name = "mi tom", price = 20000),
@@ -33,34 +38,55 @@ class MainActivity : AppCompatActivity() {
             Food(name = "mien", price = 100000)
         )
 
-        val rcvFood = findViewById<RecyclerView>(R.id.rcvFood)
-        val btnAdd = findViewById<Button>(R.id.btnClick)
+        val adapter = FoodAdapter()
+        adapter.onItemClick = {
 
-        val foodAdapter = FoodAdapter(list)
-        btnAdd.setOnClickListener {
-//            list.add(Food("hu tiu 2", 200000))
-//            list.removeAt(2)
-            viewModel.insertFood(Food(name = "Com chay", price = 60002))
-
-//            foodAdapter.notifyItemInserted(list.size - 1)
-//            foodAdapter.notifyItemChanged(2)
         }
+        binding.rcvFood.adapter = adapter
 
-        foodAdapter.onItemClick = { food ->
-            Toast.makeText(this, "$food", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, DetailActivity::class.java)
 
-            intent.putExtra("name", food)
-            startActivity(intent)
+        viewModel.listFood.observe(this){
+            adapter.submitList(it)
         }
 
 
-        viewModel.listFood.observe(this) {
-            val foodsAdapter = FoodAdapter(it ?: emptyList())
-            rcvFood.adapter = foodsAdapter
+        binding.btnClick.setOnClickListener{
+            viewModel.insertFood(Food(name = "hu tiu 2", price = 3910000))
         }
 
         viewModel.fetchListFood()
+
+
+
+//
+//        val rcvFood = findViewById<RecyclerView>(R.id.rcvFood)
+//        val btnAdd = findViewById<Button>(R.id.btnClick)
+//
+//        val foodAdapter = FoodAdapter(list)
+//        btnAdd.setOnClickListener {
+////            list.add(Food("hu tiu 2", 200000))
+////            list.removeAt(2)
+//            viewModel.insertFood(Food(name = "Com chay", price = 60002))
+//
+////            foodAdapter.notifyItemInserted(list.size - 1)
+////            foodAdapter.notifyItemChanged(2)
+//        }
+//
+//        foodAdapter.onItemClick = { food ->
+//            Toast.makeText(this, "$food", Toast.LENGTH_LONG).show()
+//            val intent = Intent(this, DetailActivity::class.java)
+//
+//            intent.putExtra("name", food)
+//            startActivity(intent)
+//        }
+//
+//
+//        viewModel.listFood.observe(this) {
+//            val foodsAdapter = FoodAdapter(it ?: emptyList())
+//            rcvFood.adapter = foodsAdapter
+//        }
+//
+//        viewModel.fetchListFood()
 
 
 //        lifecycleScope.launch(Dispatchers.Default) {
--- app/src/main/java/hanu/a2_2001040222/recyleview/adapter/FoodAdapter.kt
+++ app/src/main/java/hanu/a2_2001040222/recyleview/adapter/FoodAdapter.kt
@@ -4,28 +4,74 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import hanu.a2_2001040222.recyleview.R
 import hanu.a2_2001040222.recyleview.model.Food
 
 
-class FoodAdapter(private val list: List<Food>) : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
+//class FoodAdapter(private val list: List<Food>) : RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
+//
+//    var onItemClick : (Food) -> Unit = {
+//
+//    }
+//    inner class FoodViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        private val tvPrice = view.findViewById<TextView>(R.id.tvPrice)
+//        private val tvName = view.findViewById<TextView>(R.id.tvPho)
+//
+//        init {
+//            view.rootView.setOnClickListener {
+//                onItemClick.invoke(list[layoutPosition])
+//            }
+//        }
+//        fun onBind(food: Food) {
+//            tvPrice.text = food.price.toString()
+//            tvName.text = food.name
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val itemView = inflater.inflate(R.layout.item_food, parent, false)
+//        return FoodViewHolder(itemView)
+//    }
+//
+//    override fun getItemCount(): Int = list.size
+//
+//
+//    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+//        holder.onBind(list[position])
+//    }
+//}
 
-    var onItemClick : (Food) -> Unit = {
+class FoodAdapter : ListAdapter<Food, FoodAdapter.FoodViewHolder>(FoodDiffUtil()) {
+    internal var onItemClick: (Food) -> Unit = {
 
     }
+    internal var positionSelected = -1
+
     inner class FoodViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val tvPrice = view.findViewById<TextView>(R.id.tvPrice)
         private val tvName = view.findViewById<TextView>(R.id.tvPho)
 
         init {
             view.rootView.setOnClickListener {
-                onItemClick.invoke(list[layoutPosition])
+                notifyItemChanged(positionSelected)
+                positionSelected = layoutPosition
+                notifyItemChanged(layoutPosition)
+                onItemClick.invoke(currentList[layoutPosition])
             }
         }
+
         fun onBind(food: Food) {
             tvPrice.text = food.price.toString()
             tvName.text = food.name
+            if (layoutPosition == positionSelected) {
+                itemView.setBackgroundColor(tvName.context.getColor(R.color.teal_200))
+            } else {
+                itemView.setBackgroundColor(tvName.context.getColor(R.color.purple_200))
+            }
         }
     }
 
@@ -35,10 +81,18 @@ class FoodAdapter(private val list: List<Food>) : RecyclerView.Adapter<FoodAdapt
         return FoodViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int = list.size
-
-
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
-        holder.onBind(list[position])
+        holder.onBind(currentList[position])
     }
+}
+
+class FoodDiffUtil : ItemCallback<Food>() {
+    override fun areItemsTheSame(oldItem: Food, newItem: Food): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Food, newItem: Food): Boolean {
+        return oldItem.id == newItem.id && oldItem.name == newItem.name
+    }
+
 }
